2494d9ef00b76d2a7a9bbec6782c883596ccb212
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-05-27 00:00:31 +0530

    fix(frontend): Changed the theme of the frontend

--- frontend/src/pages/Login/LoginPage.jsx
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -3,22 +3,10 @@ import "./login.css"
 
 function LoginPage() {
   return (
-    <div className="login-page">
-      
-      {/* Header */}
-      <header className="login-header">
-        <div className="logo-box">Z</div>
-        <span className="logo-text">Zenora</span>
-      </header>
-
-      {/* Main Content */}
-      <main className="login-main">
+    <div className="login-container">
+      <div className="center-panel">
         <LoginForm />
-      </main>
-
-      {/* Help Icon */}
-      <div className="help-icon">?</div>
-      
+      </div>
     </div>
   )
 }
