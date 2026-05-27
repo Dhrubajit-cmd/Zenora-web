aff81dddebd3e69da4a54759a25e14beb2076db2
Dhrubajit-cmd <dhrubajitchakravorty@gmail.com>
2026-05-28 04:06:15 +0530

    feat: remove expense prediction graph from dashboard

--- frontend/src/pages/Dashboard/DashboardPage.jsx
+++ frontend/src/pages/Dashboard/DashboardPage.jsx
@@ -1,15 +1,10 @@
 import DashboardForm from "./DashboardForm"
 import "./dashboard.css"
-import MLGraph from "../../components/MLGraph"
 
 function DashboardPage() {
   return (
     <div className="dashboard-page">
       <DashboardForm />
-
-      <div style={{marginTop: "30px" }}>
-        <MLGraph />
-      </div>
     </div>
   )
 }
